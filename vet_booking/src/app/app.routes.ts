e7ffe7dccb9c48d11a91c06f00c5182d7b0557cc
Sandra <sandradamceva@gmail.com>
2026-04-21 14:14:44 +0200

    routes

--- vet_booking/src/app/app.routes.ts
+++ vet_booking/src/app/app.routes.ts
@@ -6,5 +6,16 @@ export const routes: Routes = [
     path: '',
     loadComponent: () =>
       import('./pages/landing/landing').then((m) => m.LandingComponent)
+  },
+
+  {
+    path: 'my-bookings',
+    loadComponent: () =>
+      import('./pages/my-bookings/my-bookings').then((m) => m.MyBookings)
+  },
+  {
+    path: 'book-appointment',
+    loadComponent: () =>
+      import('./pages/book-appointment/book-appointment').then((m) => m.BookAppointment)
   }
 ];
