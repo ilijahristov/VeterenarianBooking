c02e8e01adf317f1c393ebb07dd3bbc4b9178cc9
ilijahristov <ilijahristov20@gmail.com>
2026-04-23 13:52:29 +0200

    fixed route

--- vet_booking/src/app/app.routes.ts
+++ vet_booking/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Signup } from './pages/auth/signup/signup';
 import { LandingComponent } from './pages/landing/landing';
 import { BookAppointment } from './pages/book-appointment/book-appointment';
 import { MyBookings } from './pages/my-bookings/my-bookings';
+import { BookingDetail } from './pages/booking-detail/booking-detail';
 
 
 // canActivate: [authGuard] for route protection
@@ -12,10 +13,11 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
 
-  // comment out 3 paths below to avoid auth guard protection
+  // comment out 4 paths below to avoid auth guard protection
   { path: '', component: LandingComponent, canActivate: [authGuard] },
   { path: 'book-appointment', component: BookAppointment, canActivate: [authGuard] },
   { path: 'my-bookings', component: MyBookings, canActivate: [authGuard] },
+  { path: 'booking-detail/:id', component: BookingDetail, canActivate: [authGuard] },
 
   { path: '**', redirectTo: '' },
 
